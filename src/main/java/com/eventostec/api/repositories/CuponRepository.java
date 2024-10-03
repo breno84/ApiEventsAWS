0faccbe637b5d194874cd1f7f94803133761d4d2
Breno <brenomarcio846@gmail.com>
2024-10-03 13:27:02 -0300

    feat: create project

--- src/main/java/com/eventostec/api/repositories/CuponRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eventostec.api.repositories;
-
-import com.eventostec.api.domain.coupoun.Coupon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CuponRepository extends JpaRepository<Coupon, UUID> {
-}
